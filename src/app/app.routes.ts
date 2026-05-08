f19ad0042df39bdd346ec68230b399726cc65c8a
sergiocotiazure20262-lang <sergiocotiazure20262@gmail.com>
2026-05-08 08:55:44 -0300

    Criação da página de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pages/login', //rota de navegação
+        component: Login
+    },
+    {
+        path: '', pathMatch: 'full', //rota padrão (raiz) do projeto
+        redirectTo: '/pages/login' //redirecionamento
+    }
+];
