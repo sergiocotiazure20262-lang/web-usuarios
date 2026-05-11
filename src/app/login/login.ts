cc36f46a0dec3cca724cfefd308d9033eb8ca4ef
sergiocotiazure20262-lang <sergiocotiazure20262@gmail.com>
2026-05-11 08:52:19 -0300

    Integração com a API para autenticar usuário

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  //Inicializar o HttpClient
+  http = inject(HttpClient);
+
+  //Criando o formulário
+  formLogin = new FormGroup({
+    email : new FormControl('', [Validators.required]),
+    senha : new FormControl('', [Validators.required])
+  });
+
+  //Função para capturar o evento de submit do formulário
+  autenticar() {
+    //Fazendo uma requisição HTTP POST para a API
+    this.http.post('http://localhost:3000/api/auth/login', this.formLogin.value)
+      .subscribe({ //capturando o retorno da API
+        next: (data) => { //resposta de sucesso da API
+          console.log(data);
+        },
+        error: (e) => { //resposta de erro da API
+          console.log(e.error);
+        }
+      })
+  }
+}
